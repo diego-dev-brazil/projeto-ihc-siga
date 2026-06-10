f3356b5a49199a01091f4dcbd2c02da942349c38
diego <diegoaugustooliveira3@gmail.com>
2026-06-09 23:22:26 -0300

    Protótipo 2.0
    Mudanças feitas após a primeria validação.

--- script.js
+++ script.js
@@ -8,6 +8,20 @@ function sairSistema() {
     document.getElementById('tela-login').style.display = 'flex';
 }
 
+function mostrarInicio() {
+    document.getElementById('conteudo-inicio').style.display = 'block';
+    document.getElementById('conteudo-faq').style.display = 'none';
+    document.getElementById('botao-inicio').classList.add('ativo');
+    document.getElementById('botao-faq').classList.remove('ativo');
+}
+
+function mostrarFaq() {
+    document.getElementById('conteudo-inicio').style.display = 'none';
+    document.getElementById('conteudo-faq').style.display = 'block';
+    document.getElementById('botao-inicio').classList.remove('ativo');
+    document.getElementById('botao-faq').classList.add('ativo');
+}
+
 document.getElementById('alternador-tema').addEventListener('click', function() {
     document.body.classList.toggle('modo-escuro');
     const icone = this.querySelector('i');
@@ -19,3 +33,5 @@ document.getElementById('alternador-tema').addEventListener('click', function() 
         icone.classList.add('fa-moon');
     }
 });
+
+new window.VLibras.Widget('https://vlibras.gov.br/app');
