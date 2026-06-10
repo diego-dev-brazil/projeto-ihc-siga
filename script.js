22a892be9067914ff72a43f076008eeb694ea856
diego <diegoaugustooliveira3@gmail.com>
2026-06-09 23:24:59 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,20 +8,6 @@ function sairSistema() {
     document.getElementById('tela-login').style.display = 'flex';
 }
 
-function mostrarInicio() {
-    document.getElementById('conteudo-inicio').style.display = 'block';
-    document.getElementById('conteudo-faq').style.display = 'none';
-    document.getElementById('botao-inicio').classList.add('ativo');
-    document.getElementById('botao-faq').classList.remove('ativo');
-}
-
-function mostrarFaq() {
-    document.getElementById('conteudo-inicio').style.display = 'none';
-    document.getElementById('conteudo-faq').style.display = 'block';
-    document.getElementById('botao-inicio').classList.remove('ativo');
-    document.getElementById('botao-faq').classList.add('ativo');
-}
-
 document.getElementById('alternador-tema').addEventListener('click', function() {
     document.body.classList.toggle('modo-escuro');
     const icone = this.querySelector('i');
@@ -33,5 +19,3 @@ document.getElementById('alternador-tema').addEventListener('click', function() 
         icone.classList.add('fa-moon');
     }
 });
-
-new window.VLibras.Widget('https://vlibras.gov.br/app');
